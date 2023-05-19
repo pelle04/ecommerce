b39f05850ba17b0cfa80c055acc6be3ae97aea26
pelle04 <107267826+pelle04@users.noreply.github.com>
2023-05-19 06:47:45 -0700

    img

--- sources/chkAggiungi.php
+++ sources/chkAggiungi.php
@@ -14,34 +14,71 @@ $titolo = $_GET['titolo'];
   $prezzo = $_GET['prezzo'];
   $categoria = $_GET['categoria'];
 
+  $immagine = basename( $_FILES["foto"]["name"]);
+$target_dir = "prod_images/";
+$target_file = $target_dir . basename($_FILES["foto"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 
-  #$targetDirectory = "uploads/";  // Directory di destinazione delle immagini
-  #$targetFile = $targetDirectory . basename($_FILES["foto"]["name"]);  // Percorso completo dell'immagine
-#
-  #// Verifica se il file immagine è stato caricato correttamente
-  #if (move_uploaded_file($_FILES["foto"]["tmp_name"], $targetFile)) {
-  #  echo "L'immagine è stata caricata correttamente.";
-  #} else {
-  #  echo "Si è verificato un errore durante il caricamento dell'immagine.";
-  #}
-  #$foto=$targetFile;
-#
-  #$sqlfoto="INSERT INTO foto(path) VALUES ('".$foto."')";
-  #$conn->query($sqlfoto);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["foto"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+ // Check file size
+if ($_FILES["foto"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+ }
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["foto"]["tmp_name"], $target_file)) {
+        //echo "The file ". basename( $_FILES["foto"]["name"]). " has been uploaded.";
+        if ($conn->query($sql) === TRUE) {
+            header("location: index.php");
+         } else {
+           echo "Error: " . $sql . "<br>" . $conn->error;
+         }
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+
+
 
   $sqlcercaCategoria = 'SELECT * FROM categoria WHERE nome='.$categoria;
   $resultcercaCategoria = $conn->query($sqlcercaCategoria);
   if ($resultcercaCategoria->num_rows > 0) {  
     while ($rowcercaCategoria = $resultcercaCategoria->fetch_assoc()) {
-        $idCategoria=$row["idc"];
+        $idCategoria=$row["id"];
   }}
 
-  #$sqlcercafoto = 'SELECT * FROM categoria WHERE nome='.$foto;
-  #$resultcercafoto = $conn->query($sqlcercafoto);
-  #if ($resultcercafoto->num_rows > 0) {  
-  #  while ($rowcercafoto = $resultcercafoto->fetch_assoc()) {
-  #      $idfoto=$row["idf"];
-  #}}
+  $sqlcercafoto = 'SELECT * FROM categoria WHERE nome='.$immagine;
+  $resultcercafoto = $conn->query($sqlcercafoto);
+  if ($resultcercafoto->num_rows > 0) {  
+    while ($rowcercafoto = $resultcercafoto->fetch_assoc()) {
+        $idfoto=$row["id"];
+  }}
     $idfoto=6;
   $sqlcategoria="INSERT INTO prodotto(titolo,descrizione, venditore, quantitaMag, prezzo, IdCategoria, IdFoto) VALUES ('$titolo','$descrizione','$venditore','$quantita','$prezzo','$idCategoria','$idfoto')";
   $conn->query($sqlcategoria);
